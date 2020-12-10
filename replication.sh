fc5c096a930ec4d1417608a0ecdddeae06dcb8d5
chashakala <42860183+chashakala@users.noreply.github.com>
2020-12-10 14:32:14 +1100

    Create replication.sh
    migration

---
+++ replication.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O ./installer_linux.py https://console.cloudendure.com/installer_linux.py
+sudo python ./installer_linux.py -t CC55-80D5-9202-4B31-59E3-0AF1-FB41-B927-5DFD-A1F6-8201-EA04-29DE-7F20-7656-5200 --no-prompt
